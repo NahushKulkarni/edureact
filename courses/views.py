162fd55cfa75d0f663a827bb3ddee0c52ea4646f
https://github.com/NahushKulkarni <nahushkulkarni126@gmail.com>
2020-08-17 11:26:10 +0530

    Views Updated

--- courses/views.py
+++ courses/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from .models import Video,Course
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.generic import TemplateView
+from django.http import request
+from users.models import Student,Teacher
 # Create your views here.
 @xframe_options_exempt
 def VideoView(request):
     context={
-        'videos':Video.objects.all()
+        'course':Video.objects.get(course=request.get('C',default=None))
+        'video':Video.objects.get(video_title=request.get('V',default=None))
+        # 'user':Student.objects.get(username=request.get('U',default=None))
+        'user':Student.objects.get(username=request.user.username))
     }
     return render(request,"courses/videos.html",context)
 
 def CoursesView(request):
     context={
         'courses':Course.objects.all()
+        # 'user':Student.objects.get(username=request.get('U',default=None))
+        'user':Student.objects.get(username=request.user.username))
     }
     return render(request,"courses/courses.html",context)
